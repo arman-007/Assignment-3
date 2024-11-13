8aab2c8a8d22892df7789542e3848dc0c86d2bf2
arman-007 <armanhosen9@gmail.com>
2024-11-13 15:27:59 +0600

    final commit - project completed with updated README

--- src/routes/hotelRoutes.ts
+++ src/routes/hotelRoutes.ts
@@ -1,15 +1,11 @@
 import express, { Request, Response, Router } from 'express';
 import {hotelController} from '../controllers/hotelController';
-// import multer from 'multer';
 
 const router: Router = express.Router();
-// const upload = multer({ dest: 'src/images/' });
-
-// router.get('/', (req: Request, res: Response) => {res.send('Hello, World!')});
 
 router.get('/hotel', hotelController.getAllHotelIdsAndTitles);
 router.post('/hotel', hotelController.addHotel);
-router.post('/hotel/images', hotelController.uploadImages);
+router.post('/images', hotelController.uploadImages);
 router.get('/hotel/:hotelId', hotelController.getHotelById);
 router.put('/hotel/:hotelId', hotelController.updateHotel);
 router.delete('/hotel/:hotelId', hotelController.deleteHotelById);
